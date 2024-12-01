e42b34683adba6bff74043057d2c83cf4dad3e64
leon.schwarzer <leon.schwarzer@tu-dortmund.de>
2024-12-01 18:27:54 +0100

    Solve Day01 and add template for Day02

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,53 @@
+import kotlin.math.absoluteValue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val left : MutableList<Int> = mutableListOf()
+        val right : MutableList<Int> = mutableListOf()
+        for (line : String in input) {
+            val elem = line.split("   ")
+            left.add(elem[0].toInt())
+            right.add(elem[1].toInt())
+        }
+        left.sort()
+        right.sort()
+        var result = 0
+        for (i : Int in input.indices) {
+            result += (left[i] - right[i]).absoluteValue
+        }
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val left : MutableList<Int> = mutableListOf()
+        val right : MutableList<Int> = mutableListOf()
+        val numberToCount : MutableMap<Int, Int> = mutableMapOf()
+        for (line : String in input) {
+            val elem = line.split("   ")
+            left.add(elem[0].toInt())
+            val rightNumber = elem[1].toInt()
+            right.add(rightNumber)
+            if (!numberToCount.containsKey(rightNumber)){
+                numberToCount[rightNumber] = 1
+            }
+            else {
+                numberToCount[rightNumber] = numberToCount[rightNumber]!! + 1
+            }
+        }
+        var result = 0
+        for (number in left) {
+            val count : Int = if (numberToCount.containsKey(number)) numberToCount[number]!! else 0
+            result += number * count
+        }
+
+        return result
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    val testInput = readInput("inputs/Day01_test")
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput("inputs/Day01")
     part1(input).println()
     part2(input).println()
 }
