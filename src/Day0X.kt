9bd024cff3d4316b38f9ae45414e9b8b8e692b76
leon.schwarzer <leon.schwarzer@tu-dortmund.de>
2024-12-05 18:27:08 +0100

    Template for base structure

---
+++ src/Day0X.kt
@@ -0,0 +1,18 @@
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = readInput("inputs/Day0X_test")
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
+
+    val input = readInput("inputs/Day0X")
+    part1(input).println()
+    //part2(input).println()
+}
